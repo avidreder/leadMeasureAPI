430a35ca8b952bbda6d49ca02e63a824c150bbba
Andrew Reder <avidreder@gmail.com>
2017-07-28 15:13:08 -0700

    added basic routes and dockerfile

---
+++ src/main/java/leadAPIRoutes/Routes.java
@@ -0,0 +1,10 @@
+package leadAPIRoutes;
+
+import static spark.Spark.*;
+
+public class Routes {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+        get("/healthCheck", (req, res) -> "I am a health check");
+    }
+}
