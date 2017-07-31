1975a514c773897fee2e5cc1a37467b2eb928e31
Andrew Reder <avidreder@gmail.com>
2017-07-31 10:25:05 -0700

    update to trigger build

--- src/main/java/leadAPIRoutes/Routes.java
+++ src/main/java/leadAPIRoutes/Routes.java
@@ -5,6 +5,6 @@ import static spark.Spark.*;
 public class Routes {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World");
-        get("/healthCheck", (req, res) -> "I am a health check");
+        get("/healthCheck", (req, res) -> "I am a health check Yayyy");
     }
 }
