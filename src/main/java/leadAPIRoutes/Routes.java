b9e61ade7e68382063fa507ffaa33b67b469e44d
Andrew Reder <avidreder@gmail.com>
2017-08-03 12:44:54 -0700

    added integration tests

--- src/main/java/leadAPIRoutes/Routes.java
+++ src/main/java/leadAPIRoutes/Routes.java
@@ -4,8 +4,11 @@ import static spark.Spark.*;
 
 public class Routes {
     public static void main(String[] args) {
-
+        get("/hello", (req, res) -> "Hello world");
         get("/healthCheck", (req, res) -> "I am a health check Yayyy");
-        notFound((req, res) -> halt(404));
+        notFound((req, res) -> {
+            res.status(404);
+            return "Not Found";
+        });
     }
 }
--- src/test/java/leadAPIRoutes/RouteIntegrationTest.java
+++ src/test/java/leadAPIRoutes/RouteIntegrationTest.java
@@ -3,73 +3,62 @@ package leadAPIRoutes;
 import leadAPIRoutes.Routes;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import org.junit.After;
 import spark.utils.IOUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.experimental.categories.Category;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
-import java.util.List;
-import java.util.Map;
 
-import static spark.Spark.after;
-import static spark.Spark.before;
 import spark.Spark;
 
 public class RouteIntegrationTest {
-    private static int PORT = 4567;
+
+    @BeforeClass
+    public static void beforeClass() {
+        Routes.main(null);
+        Spark.awaitInitialization();
+    }
+
+    @Test
+    public void helloRouteReturns200Response() {
+        TestResponse res = request("GET", "/hello");
+        assertEquals(200, res.status);
+    }
+
+    @Test
+    public void healthCheckRouteReturns200Response() {
+        TestResponse res = request("GET", "/healthCheck");
+        assertEquals(200, res.status);
+    }
+
+    @Test
+    public void nonExistentReturns404Response() {
+        TestResponse res = request("GET", "/badRoute");
+        assertEquals(404, res.status);
+    }
 
     @AfterClass
     public static void tearDown() {
         Spark.stop();
     }
 
-//    @After
-//    public void clearBooks() {
-//        Books.books.clear();
-//    }
-
-    @BeforeClass
-    public static void beforeClass() {
-//        before((request, response) -> {
-//            response.header("FOZ", "BAZ");
-//        });
-
-        Routes.main(null);
-
-//        after((request, response) -> {
-//            response.header("FOO", "BAR");
-//        });
-
-//        Spark.awaitInitialization();
-    }
-
-    @Test
-    public void testHelloRoute() {
-        TestResponse res = request("GET", "/hello");
-        System.out.print(res.status);
-        assertEquals(200, res.status);
-    }
-
     private TestResponse request(String method, String path) {
         try {
             URL url = new URL("http://localhost:4567" + path);
-            System.out.println(url);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod(method);
             connection.setDoOutput(true);
             connection.connect();
-            String body = IOUtils.toString(connection.getInputStream());
+            int status = connection.getResponseCode();
+            String body = null;
+            if (status != 404) {
+                body = IOUtils.toString(connection.getInputStream());
+            }
             return new TestResponse(connection.getResponseCode(), body);
         } catch (IOException e) {
             e.printStackTrace();
-//            fail("Sending request failed: " + e.getMessage());
             return null;
         }
     }
@@ -83,10 +72,6 @@ public class RouteIntegrationTest {
             this.status = status;
             this.body = body;
         }
-
-//        public Map<String,String> json() {
-//            return new Gson().fromJson(body, HashMap.class);
-//        }
     }
 }
 
